cfca6b8c9de98c5ee9b8ff0b4ae1924b519757e8
ywkim <akr1253@naver.com>
2021-12-15 01:20:45 +0900

    Create dishes.py

---
+++ Stack/dishes.py
@@ -0,0 +1,36 @@
+string = input()
+ln = len(string)
+inputs = list(string)
+inputs.sort()
+
+inp = inputs.pop(0)
+stack = [inp]
+result = [1,]
+answer = ''
+tag = 0
+flag = False
+for _ in range(1, 2*ln):
+  if not stack:
+    inp = inputs.pop(0)
+    stack.append(inp)
+    result.append(1)
+    continue
+  if stack[-1]==string[tag]:
+    p = stack.pop()
+    answer+=p
+    tag+=1 
+    result.append(-1)
+  else:
+    if string[tag] in stack[:-1]:
+      print('impossible')
+      flag  =True
+      break
+    else:
+      inp = inputs.pop(0)
+      stack.append(inp)
+      result.append(1)
+if not flag:
+  for r in result:
+    if r==1: print('push')
+    else: print('pop')
+    
