127cc3d53f80fd0dc11bb090009f614697a154cd
ywkim <akr1253@naver.com>
2021-12-21 23:56:54 +0900

    Create nn_table.py

---
+++ Binary_search/nn_table.py
@@ -0,0 +1,40 @@
+num, = map(int, input().split())
+k, = map(int, input().split())
+
+start = 1
+end = num**2
+
+# n보다 작거나 같은 수의 개수?
+def getOrder(n):
+    tag = 1
+    count = 0
+    mn = min(num, n)
+    while tag<=mn:
+        ans = n//tag
+        if ans>=num: ans=num
+        count+=ans
+        tag+=1
+    return count
+
+def getMax(n):
+    tag = 1
+    mn = min(num, n)
+    mx = 0
+    while tag<=mn:
+        ans = n//tag
+        if ans>=num: ans=num
+        if ans*tag>mx: mx = ans*tag
+        tag+=1
+    return mx
+
+found = False
+while start<=end and not found:
+    mid = (start+end)//2
+    go = getOrder(mid)
+    if go==k:
+        print(getMax(mid))
+        found = True
+    else:
+        if go>k: end = mid - 1
+        else: start = mid +1
+if not found: print(getMax(start))
