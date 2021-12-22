7ffa550a2b6cc7e1cce4d14b06762cf039a83d38
ywkim <akr1253@naver.com>
2021-12-22 23:52:52 +0900

    Update acid_alkaline.py

--- Binary_search/acid_alkaline.py
+++ Binary_search/acid_alkaline.py
@@ -1,14 +1,11 @@
-def binary_search1(array, n):
-    found = False
-    first = 0
-    last = len(array) - 1
-    while first<=last and not found:
-        mid = (first+last)//2
-        if array[mid]==n: found= True
-        elif array[mid]>n: last= mid - 1
-        else: first = mid + 1
-    return mid, first, last
-
+'''
+두 용액
+input
+5
+-2 4 -99 1 98
+output
+-99 98
+'''
 def binary_search(array, n):
     found = False
     first = 0
@@ -23,7 +20,7 @@ def binary_search(array, n):
 num, = map(int, input().split())
 array = list(map(int, input().split()))
 array.sort()
-# print(binary_search1([-100, -50, 20, 40, 80], 50))
+
 if len(array)==2: print(*array)
 elif array[-1]<0: print(*array[-2:])
 elif array[0]>0: print(*array[:2])
@@ -59,4 +56,3 @@ else:
   answer = sorted(answer, key =lambda x: (x[2], x[0]), )
   # print(answer)
   if not flag: print(*answer[0][:2])
-      
